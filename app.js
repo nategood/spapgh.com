9ef8993a697257648d7762a4435666e454be0131
Nathan <nate.good@showclix.com>
2015-01-27 12:39:58 -0500

    email

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post('/contact-metamorphosis', function(req, res) {
       service: 'Gmail',
       auth: {
           user: 'staff@spapgh.com',
-          pass: 'meta 5112'
+          pass: 'a profound change'
       }
   });
   var name = req.param('name'),
